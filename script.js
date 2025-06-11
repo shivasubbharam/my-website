553c1b63caf0d7245b20eb4126385fd7e97dd800
shivasubbharam <56816464+shivasubbharam@users.noreply.github.com>
2025-06-11 10:25:48 +0530

    Add files via upload
    Website with login

---
+++ script.js
@@ -0,0 +1,14 @@
+function login(event) {
+  event.preventDefault();
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  const message = document.getElementById("message");
+
+  if (username === "admin" && password === "admin123") {
+    message.style.color = "green";
+    message.innerText = "Login successful!";
+  } else {
+    message.style.color = "red";
+    message.innerText = "Invalid credentials! Try admin / admin123.";
+  }
+}
