cb8de6f73f23517b689a88358af621e4bc9adbdc
shivasubbharam <56816464+shivasubbharam@users.noreply.github.com>
2025-06-12 18:55:42 +0530

    Update script.js
    cc

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function login(event) {
   event.preventDefault();
-  const username = document.getElementById("username").value;
+  const username = document.getElementById("usernameguru").value;
   const password = document.getElementById("password").value;
   const message = document.getElementById("message");
 
